c1cf6151dad8d4d02366c7a240ee724bca638819
MikeCAT <mikecat@users.noreply.github.com>
2018-02-09 17:24:00 +0900

    add Activity.onCreate() log

--- app/src/main/java/f483609b_5c84_45c7_b1a6_5e3ee1871bea/viewpagertest/MainActivity.java
+++ app/src/main/java/f483609b_5c84_45c7_b1a6_5e3ee1871bea/viewpagertest/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("activity", "onCreate()");
 
         final List<String> itemList = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
